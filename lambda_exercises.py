53c362e37b1225b75a345d7e53b728af1ff54d1f
Stiles Clements <Stiles_Clements1@baylor.edu>
2023-10-31 17:21:48 -0500

    Add HW

--- lambda_exercises.py
+++ lambda_exercises.py
@@ -10,6 +10,13 @@ Odd numbers from the said list:
 
 
 
+original_list=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+even_list=list(filter(lambda num:num%2==0,original_list))
+print(even_list)
+odd_list=list(filter(lambda num:num%2==1,original_list))
+print(odd_list)
+
 
 
 ''' 2)
@@ -21,7 +28,8 @@ weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 
 
 
 
-
+char6_list=list(filter(lambda word:len(word)==6,weekdays))
+print(char6_list)
 
 
 
@@ -38,11 +46,11 @@ After removing the specified words from the said list:
 ['red', 'green', 'blue', 'white']
 
 '''
+original_list=['orange', 'red', 'green', 'blue', 'white', 'black']
+remove_words=['orange', 'black']
 
-
-
-
-
+shortened_list=list(filter(lambda word:word not in remove_words,original_list))
+print(shortened_list)
 
 
 
@@ -58,9 +66,11 @@ Remove all elements from 'list1' present in 'list2:
 [1, 3, 5, 7, 9, 10]
  '''
 
+list1= [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+list2= [2, 4, 6, 8]
 
-
-
+new_list=list(filter(lambda num:num not in list2,list1))
+print(new_list)
 
 
 ''' 5)
@@ -77,10 +87,11 @@ Elements of the said list that contain specific substring:
 []
 
 '''
+def search(search):
+    print(list(filter(lambda word: search in word, original_list)))
 
-
-
-
+search('ack')
+search('abc')
 
 
 ''' 6)
@@ -88,10 +99,24 @@ check whether a given string contains a capital letter, a lower case letter, a n
 (This is like a password verification function, HINT: Python function 'any' may be useful)
 '''
 
-str1 = "Hello8world"
-str1 = "HELLO"
-str1= "hello"
+def pass_check(password):
+    check_list=[]
+    funct=lambda function:check_list.append(function)
+    funct(password.islower())
+    funct(password.isupper())
+    funct(password.isalpha())
+    funct(len(password)<=8)
+    if any(check_list)==False:
+        print(f"{password} is an acceptable password")
+    else:
+        print(f"{password} is not an acceptable password")
 
+str1 = "Hello8world"
+pass_check(str1)
+str1 = "HELLO"
+pass_check(str1)
+str1= "hello"
+pass_check(str1)
 
 
 
@@ -112,3 +137,9 @@ original_scores = [('English', 88), ('Science', 90), ('Maths', 97), ('Social sci
 # Expected Result:
 # [('Social sciences', 82), ('English', 88), ('Science', 90), ('Maths', 97)]
 '''
+
+
+original_scores = [('English', 88), ('Science', 90), ('Maths', 97), ('Social sciences', 82)]
+secondval=lambda key:key[1]
+original_scores.sort(key=secondval)
+print(original_scores)
--- list_comprehension_start_file.py
+++ list_comprehension_start_file.py
@@ -10,6 +10,8 @@ context of the for and if clauses which follow it.
 The list comprehension always returns a result list. '''
 
 
+# Expression/transformation, iteration, condition/filter
+
 '''
 new_list = []
 for i in original_list:
@@ -32,6 +34,62 @@ for i in original_list:
 
 
 
+
+
+
+x=[i for i in range(10)]
+print(x)
+
+#add expression
+
+squares=[x**2 for x in range(10)]
+print(squares)
+
+list1=[3,4,5]
+
+multiplied=[item*3 for item in list1]
+print(multiplied)
+
+
+listOfWords=['this','is','a','list','of','words']
+
+result=[word[0].upper() for word in listOfWords]
+print(result)
+
+
+#adding in an IF condition
+# a new list of the square of all even number from 1 to 10
+even_squares=[num**2 for num in range(1,11) if num%2==0]
+print(even_squares)
+
+
+string="Hello 12345 World"
+numbers=[int(x) for x in string if x.isdigit()]
+print(numbers)
+
+
+list1=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+list2=[]
+
+list2=[x*100 for x in list1 if x%2==1]
+print(list2)
+
+
+infile=open('test.txt','r')
+result=[x.rstrip('\n') for x in infile if 'line3' in x]
+print(result)
+
+def double(x):
+    return x*2
+print(double(10))
+
+
+result=[double(x) for x in range(10) if x%2==0]
+print(result)
+
+result=[x+y for x in [10,30,50] for y in [20,40,60]]
+print(result)
+
 ## Exercise ##
 
 # 1 Using a list comprehension, create a new list called "newlist" out of the list "numbers", 
@@ -39,8 +97,8 @@ for i in original_list:
 
 numbers = [34.6, -203.4, 44.9, 68.3, -12.2, 44.6, 12.7]
 
-
-
+newlist=[x for x in numbers if x>0]
+print(newlist)
 
 ## 2 create a list of integers which specify the length of each word in
 ## a sentence except for the word 'the'
@@ -48,7 +106,8 @@ numbers = [34.6, -203.4, 44.9, 68.3, -12.2, 44.6, 12.7]
 sentence = "the quick brown fox jumps over the lazy dog"
 words = sentence.split()
 
-
+word_len=[len(x) for x in words if x!='the']
+print(word_len)
 
 ## Given dictionary is consisted of vehicles and their weights in kilograms. 
 ## Contruct a list of the names of vehicles with weight below 5000 kilograms. 
@@ -57,24 +116,37 @@ words = sentence.split()
 dict={"Sedan": 1500, "SUV": 2000, "Pickup": 2500, "Minivan": 1600, "Van": 2400, 
 "Semi": 13600, "Bicycle": 7, "Motorcycle": 110}
 
-
+light_vehicle=[k.upper() for k,v in dict.items() if v<5000]
+print(light_vehicle)
 
 
 ## Find all the numbers from 1 to 1000 that have a 4 in them
 
-
+nums_with_4=[x for x in range(1,1001) if '4' in str(x)]
+print(nums_with_4)
 
 ## count how many times the word 'the' appears in the text file - 'sometext.txt'
 
+infile=open('sometext.txt','r')
 
+
+
+count=0
+# the_count=[word for line in infile for word in line.split() if word.lower()=='the']
+# print(the_count)
+
+
+for the in [word for line in infile for word in line.split() if word.lower()=='the']:
+    count+=1
+print(count)
 
 ## Extract the numbers from the following phrase ##
 
-phrase = 'In 1984 there were 13 instances of a protest with over 1000 people attending. On average there were 15 reported injuries at each " +
-"event, with about 3 or 4 that were classifled as serious per event.'
+phrase = 'In 1984 there were 13 instances of a protest with over 1000 people attending. On average there were 15 reported injuries at each event, with about 3 or 4 that were classifled as serious per event.'
 
 
-
+digits=[word for word in phrase.split() if word.isdigit()]
+print(digits)
 
 
 
